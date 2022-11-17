89e5fc447ef75cb433765a0b919a92b9878dce35
Mahad410 <mahadqureshi410@gmail.com>
2022-11-17 13:52:54 +0500

    snap

--- main.js
+++ main.js
@@ -1,38 +1,52 @@
-const feature = document.getElementById('feature');
-const company = document.getElementById('company');
-const drop_feat = document.getElementById('drop_feature');
-const drop_comp = document.getElementById('drop_comp');
-const drop_img_feat = document.getElementById('drop_img_feat');
-const drop_img_comp = document.getElementById('drop_img_comp');
-feature.addEventListener('click',
-    ()=>{
-        if(feature.lastElementChild.classList.contains('drop_menu')){
-            drop_feat.classList.toggle('active');
-            drop_comp.classList.remove('active');
-            feature.lastElementChild.classList.toggle('drop_menu_hide');
-            feature.lastElementChild.classList.add('opacity');
-            company.lastElementChild.classList.add('drop_menu_hide');
-            drop_img_feat.classList.toggle('rotate');
-            drop_img_comp.classList.remove('rotate');
-        }
-        else{
-            return false
-        }
-    })
+const openMenu = document.getElementById('open');
+const closeMenu = document.getElementById('close');
+const menu = document.getElementById('menu');
+const side_comp = document.getElementById('side_main_comp');
+const side_feat = document.getElementById('side_main_feat');
+const sub_menu_comp = document.getElementById('sub_menu_comp');
+const sub_menu_feat = document.getElementById('sub_menu_feat');
+const main_feat = document.getElementById('main_feat');
+const main_comp = document.getElementById('main_comp');
+const main_sub_feat = document.getElementById('main_sub_menu_feat');
+const main_sub_comp = document.getElementById('main_sub_menu_comp');
+openMenu.addEventListener('click', () => {
+    menu.style.transform = 'translateX(0)';
+})
+closeMenu.addEventListener('click', () => {
+    menu.style.transform = 'translateX(100%)';
+})
 
-company.addEventListener('click',
-    ()=>{
-        if(company.lastElementChild.classList.contains('drop_menu')){
-            drop_comp.classList.toggle('active');
-            drop_feat.classList.remove('active');
-            company.lastElementChild.classList.toggle('drop_menu_hide');
-            company.lastElementChild.classList.add('opacity');
-            feature.lastElementChild.classList.add('drop_menu_hide');
-            drop_img_comp.classList.toggle('rotate');
-            drop_img_feat.classList.remove('rotate');
-        }
-        else{
-            return false;
-        }
+side_comp.addEventListener('click', () => {
+    sub_menu_comp.classList.toggle('hide_side_feature');
+    side_comp.children[0].classList.toggle('rotate');
+    side_feat.children[0].classList.remove('rotate');
+    side_comp.classList.toggle('active');
+    side_feat.classList.remove('active');
+    sub_menu_feat.classList.add('hide_side_feature');
+})
 
-    })
+side_feat.addEventListener('click', () => {
+    sub_menu_feat.classList.toggle('hide_side_feature');
+    side_feat.children[0].classList.toggle('rotate');
+    side_comp.children[0].classList.remove('rotate');
+    side_feat.classList.toggle('active');
+    side_comp.classList.remove('active');
+    sub_menu_comp.classList.add('hide_side_feature');
+})
+
+main_feat.addEventListener('click', () => {
+    main_sub_feat.classList.toggle('hide_side_feature');
+    main_feat.children[0].classList.toggle('rotate');
+    main_comp.children[0].classList.remove('rotate');
+    main_feat.classList.toggle('active');
+    main_comp.classList.remove('active');
+    main_sub_comp.classList.add('hide_side_feature');
+})
+main_comp.addEventListener('click', () => {
+    main_sub_comp.classList.toggle('hide_side_feature');
+    main_comp.children[0].classList.toggle('rotate');
+    main_feat.children[0].classList.remove('rotate');
+    main_comp.classList.toggle('active');
+    main_feat.classList.remove('active');
+    main_sub_feat.classList.add('hide_side_feature');
+})
